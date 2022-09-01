1e4fe13b3da93cdc068564e43c959268b144b5bd
jaleel24 <36284471+jaleel24@users.noreply.github.com>
2022-09-01 11:18:07 -0400

    added router and now we can navigate to  header and hero page with Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import React, { Fragment } from 'react';
 import Header from './components/header';
 import Hero from './components/hero';
 import data from './data/data.json';
+import {Route , Switch, Redirect} from 'react-router-dom';
 
 function App() {
   return (
    <Fragment>
-     <Header />
-    <Hero data={data.Header}/>
+    <Header />
+    <Switch>
+    {/* <Route path='/'></Route> */}
+    <Route path='/' exact><Redirect to="/home" /></Route>
+     
+   <Route path='/home' exact><Hero data={data.Header}/></Route>
+   </Switch>
    </Fragment>
+   
   );
 }
 
